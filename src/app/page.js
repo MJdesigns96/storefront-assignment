ffc7b4e9779d27c05b2e2b31fe807a56ee13dd88
Marcus Jeong <marcus.jeong96@gmail.com>
2024-12-13 00:38:26 -0500

    added image carousel to home page

--- src/app/page.js
+++ src/app/page.js
@@ -14,12 +14,15 @@ export default async function Home() {
             <CarouselItem>
                 <img className="d-block w-100 object-fit-cover" style={{height: "75vh"}} src={waterBottle.src} alt="Landing Picture" />
             </CarouselItem>
+            {/* https://unsplash.com/photos/bear-illustration-on-orange-tumbler-nWy_OyaAWdU */}
             <CarouselItem>
                 <img className="d-block w-100 object-fit-cover" style={{height: "75vh"}} src={citrus.src} alt="citrus suspended in water" />
             </CarouselItem>
+            {/* https://unsplash.com/photos/sliced-orange-fruits-underwater-qpN5MLx1uwk */}
             <CarouselItem>
                 <img className="d-block w-100 object-fit-cover" style={{height: "75vh"}} src={drop.src} alt="a drop of water into a body of water" />
             </CarouselItem>
+            {/* https://unsplash.com/photos/water-drops-macro-photography-XSQHuGGRO3g */}
           </Carousel>
         </div>
       </div>
