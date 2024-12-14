6c34e7a31444986b3e835c8ac129de213cd76ae2
Marcus Jeong <marcus.jeong96@gmail.com>
2024-12-14 14:17:42 -0500

    finished footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,26 +3,25 @@ import Link from "next/link"
 export default function Footer() {
     return(
         <footer className="text-center text-lg-start bg-body-tertiary text-muted">
-            <hr></hr>
-            <section>
-                <div className="row mt-3">
-                    <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                        <div className="row">
-                            <h1 className="text-center">
-                                <Link href="/">Storefront</Link>
-                            </h1>
-                            <p>blurb about company tagline</p>
-                            <nav className="navbar navbar-expand-lg">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item"><Link className="nav-link" href="/products">Products</Link></li>
-                                    <li className="nav-item"><Link className="nav-link" href="/stores">Stores</Link></li>
-                                    <li className="nav-item"><Link className="nav-link" href="/about">About Us</Link></li>
-                                </ul>
-                            </nav>
-                            <p>Copyright</p>
-                        </div>
+            <section className="py-5 spacer mx-0 px-5">
+                <div className="row py-5" id="footer">
+                    <div className="col text-center">
+                        <h1>
+                            <Link className="navbar-brand" href="/">H2Water</Link>
+                        </h1>
+                        <p>Saving the world One bottle at time</p>
                     </div>
-                </div>         
+                    <div className="col">
+                        <nav className="navbar navbar-expand-lg">
+                            <ul className="navbar-nav">
+                                <li className="nav-item"><Link className="nav-link" href="/products">Products</Link></li>
+                                <li className="nav-item"><Link className="nav-link" href="/stores">Stores</Link></li>
+                                {/* <li className="nav-item"><Link className="nav-link" href="/about">About Us</Link></li> */}
+                            </ul>
+                        </nav>
+                        <p>&copy; Copyright HTTP5222 Marcus Jeong, 2024</p>
+                    </div>
+                </div>  
             </section>
               
         </footer>
